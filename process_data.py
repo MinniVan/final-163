910cbe2bd366be45a125ec58b004f6c240d0e041
buruneve <69091730+buruneve@users.noreply.github.com>
2020-08-21 21:09:11 -0700

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -1,17 +1,15 @@
-
 import h5py
 import pandas as pd
 
+
 class Data:
-
-
-
     def __init__(self, file_name):
         _data = h5py.File(file_name, 'r+')
         _tree_array = _data['t_allpar_new'][()]
 
         _data_df = pd.DataFrame(_tree_array)
-        self._filtered_data = _data_df.drop_duplicates(subset=[('j_index') or ('index')]) 
+        self._filtered_data = _data_df.drop_duplicates(subset=[('j_index')
+                                                       or ('index')])
 
         self._array_keys = _tree_array.dtype.names
 
@@ -25,7 +23,6 @@ class Data:
         for feat in hl_features:
             result[feat] = self._filtered_data[feat]
         return list(result.keys())
-    
 
     def get_low_features(self):
         ll_features = [i for i in self._array_keys if (i[0:3] == 'j1_')]
@@ -34,16 +31,9 @@ class Data:
             result[feat] = self._filtered_data[feat]
         return list(result.keys())
 
-
     def get_labels_truth_table(self):
-        keys_filter = [i for i in self._array_keys if (i[0:2] == 'j_')]
         labels = ['j_g', 'j_q', 'j_w', 'j_z', 'j_t', 'j_undef']
         result = dict()
         for feat in labels:
             result[feat] = self._filtered_data[feat]
         return list(result.keys())
-
-
-    
-
-        
