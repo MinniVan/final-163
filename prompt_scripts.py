2e4326cedf72f4fcba11a9d6646b1df7d85039a5
buruneve <69091730+buruneve@users.noreply.github.com>
2020-08-21 21:24:19 -0700

    Update prompt_scripts.py

--- prompt_scripts.py
+++ prompt_scripts.py
@@ -1,9 +1,10 @@
 # This program takes in user input on what features they would like to plot
-
+# Whether it is for high or low level features
 
 
 def intro():
-    
+    # This method introduces the method to the user.
+    # And informs them of what they should be expected to do.
     print('In this program we will be exploring a Dense Neural Network (DNN)',
           'algorithm for jet tagging in high energy physics. Specifically',
           'we will analyze two type of features that would be used as input',
@@ -17,21 +18,21 @@ def intro():
           'low level features.')
     print()
 
+
 def choose_file():
+    # This method asks user which data file they would like to use
+    # They have two options: the larger or smaller dataset
     samples = {'1': 'larger_sample.z',
                 '2': 'smaller_sample.z'}
-    
     print('There are two files in here')
     print('     1. Big Sample (aprox (1.3GB)')
     print('     2. Smaller Sample (aprox. 500mb)')
     print()
     print('Specify which sample you would like us to load.')
     print()
-    
     while True:
         user_input = input('Choose the file (Type 1 or 2): ')
         print()
-
         if user_input == '1' or user_input == '2':
             return samples[user_input]
         else:
@@ -40,15 +41,17 @@ def choose_file():
 
 
 def levels():
+    # This method asks user which level feature they are 
+    # interested in viewing
+    # There are two options: high or low level features
     print()
     print('There are two level features:')
     print(' 1. High Level')
     print(' 2. Low Level')
     print()
-
     while True:
-        type_level = input('Which level feature are you interested in seeing? (Type 1 or 2): ')
-
+        type_level = input('Which level feature are you interested in seeing?',
+                           '(Type 1 or 2): ')
         if type_level == '1' or type_level == '2':
             return int(type_level)
         else:
@@ -56,17 +59,20 @@ def levels():
             print()
 
 def hlf():
-    
+    # If the user chose high level features
+    # They are then prompted to choose one of the three
+    # categories for high level features
     print('Here are the different types of High Level Features:')
     print('     1. Jet Kinematics')
     print('     2. Jet Substructures')
     print('     3. Energy-Correlation Functions')
     print()
-    print('Specify which group of High Level feature you would like to look at?')
+    print('Specify which group of High Level feature you would like to 
+          'look at?')
     print()
-
     while True:
-        user_input = input('Enter the number associated with a High Level Feature type: ')
+        user_input = input('Enter the number associated with a High Level',
+                           'Feature type: ')
         possible_ins = {'1', '2', '3'}
         if user_input in possible_ins:
             return int(user_input)
@@ -75,7 +81,9 @@ def hlf():
             print()
     
 def llf():
-
+    # If user chose low level features
+    # they are then prompted to choose one of seven
+    # features to plot
     print()
     print('Here are the different types of Low Level Features:')
     print('     1. PDF ID number of the constituent')
@@ -92,7 +100,8 @@ def llf():
 
     while True:
         print()
-        user_input = input('Enter the number associated with a Low Level Feature type: ')
+        user_input = input('Enter the number associated with a Low Level',
+                           'Feature type: ')
         possible_ins = {'1', '2', '3', '4', '5', '6', '7'}
         if user_input in possible_ins:
             return int(user_input)
@@ -101,13 +110,15 @@ def llf():
             print()
             
 def hl_kinematics():
+    # If use chose jet kinematics for high level features
+    # they are then prompted to chose a specific feature to plot
     print()
     print('Jet kinematics:')
     print('     1. Jet mass computed based on modified mass drop tagger')
     print('     2. Jet pT (transverse momentum)')
-
     while True:
-        user_input = input('Enter the number associated with a Low Level Feature type: ')
+        user_input = input('Enter the number associated with a Low Level',
+                           'Feature type: ')
         possible_ins = {'1', '2'}
         if user_input in possible_ins:
             return int(user_input)
@@ -116,12 +127,15 @@ def hl_kinematics():
             print()
             
 def hl_jetsub():
+    # if user chose jet substructures as a high level feature
+    # they are then prompted to chose a specific feature to plot
     print('Jet subsctructures')
     print('     1. Jet splitting fraction')
     print('     2. Number of constituents')
 
     while True:
-        user_input = input('Enter the number associated with a Low Level Feature type: ')
+        user_input = input('Enter the number associated with a Low Level',
+                           'Feature type: ')
         possible_ins = {'1', '2'}
         if user_input in possible_ins:
             return int(user_input)
@@ -130,6 +144,8 @@ def hl_jetsub():
             print()
 
 def hl_energy():
+    # if user chose energu correlation funcitons as a high level feature
+    # they are thne prompted to pick a specific feature 
     print('Energy-Correlation functions')
     print('     1. j_c1_b0_mmdt')
     print('     2. j_c1_b1_mmdt')
@@ -144,9 +160,9 @@ def hl_energy():
     print('     11. j_m2_b2_mmdt')
     print('     12. j_n2_b1_mmdt') 
     print('     13. j_n2_b2_mmdt')
-    
     while True:
-        user_input = input('Enter the number associated with a Low Level Feature type: ')
+        user_input = input('Enter the number associated with a Low Level',
+                           'Feature type: ')
         possible_ins = {'1', '2', '3', '4', '5', '6', '7',
                         '8', '9', '10', '11', '12', '13'}
         if user_input in possible_ins:
